c854364a8e056ce5b8a0089619a77994e2028f47
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-06-04 17:47:52 +0300

    prepare fo fedora 22
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -ex
 
-yum update
+yum update || true
 
 # Remove unnecessary packages (and their dependencies)
 # which can’t be removed until after the installation process
 yum --assumeyes autoremove authconfig firewalld linux-firmware
 
 # Clean up old yum repo data & logs
-yum clean all
-yum history new
-rm --recursive --force /var/lib/yum/yumdb/*
-rm --recursive --force /var/lib/yum/history/*
-truncate --no-create --size=0 /var/log/yum.log
+yum clean all || true
+yum history new || true
+rm --recursive --force /var/lib/yum/yumdb/* || true
+rm --recursive --force /var/lib/yum/history/* || true
+truncate --no-create --size=0 /var/log/yum.log || true
 
 # Remove random-seed, so it’s not the same in every image
 rm --force /var/lib/random-seed
@@ -21,6 +21,6 @@ dracut -H --force
 # Change any incorrect SELinux context labels
 fixfiles -R -a restore
 
-rm -rf /tmp/* /root/anaconda-ks.cfg /root/install.log /root/install.log.syslog /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /tmp/* /root/anaconda-ks.cfg /root/install.log /root/install.log.syslog /etc/udev/rules.d/70-persistent-net.rules || true
 
 fstrim -v /
